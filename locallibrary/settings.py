60fefa1083f10efcc81f654f7db16731a420e4e4
matthewnolan <matthewnolan415@gmail.com>
2017-04-07 04:11:15 -0400

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '5mv(t^&o*$h=nck+uu8xztqv0c+5b#
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['murmuring-harbor-69377.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
