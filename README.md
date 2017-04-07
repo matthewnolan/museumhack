8876ad9b7cff38952f77258f6bca5f48c4088b0f
matthewnolan <matthewnolan415@gmail.com>
2017-04-07 03:56:56 -0400

    Added files and changes required for deployment to heroku

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 From this
 
 https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django
+
+Simple Django Ajax:
+
+https://simpleisbetterthancomplex.com/tutorial/2016/08/29/how-to-work-with-ajax-request-with-django.html
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5mv(t^&o*$h=nck+uu8xztqv0c+5b#ajg85dg%=pnjr130062b'
+# SECRET_KEY = '5mv(t^&o*$h=nck+uu8xztqv0c+5b#ajg85dg%=pnjr130062b'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '5mv(t^&o*$h=nck+uu8xztqv0c+5b#ajg85dg%=pnjr130062b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +127,23 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
