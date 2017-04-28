28bbe4bb599908aaac295acef69faf08ddcedba5
matthewnolan <matthewnolan415@gmail.com>
2017-04-28 16:03:29 -0400

    added domain

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '5mvt^&o*$h=nck+uu8xztqv0c+5b#ajg85dg%=pnjr130062b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['museumpatrons.com', 'museumhack.herokuapp.com', '127.0.0.1']
