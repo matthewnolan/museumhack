712b81c14ab8184ee4a86fe7894d74345ed0017d
matthewnolan <matthewnolan415@gmail.com>
2017-06-10 22:08:48 -0400

    removed unusedfiles

--- catalog/serializers.py
+++
@@ -1,9 +0,0 @@
-# TODO use API or remove it
-from rest_framework import serializers
-# from .models import Institution, LANGUAGE_CHOICES, STYLE_CHOICES
-from .models import Institution
-
-class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Institution
-        fields = ('id', 'name', 'city')
